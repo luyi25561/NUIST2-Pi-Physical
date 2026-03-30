1ab5c3654e461d2ee37af6c9cfc4883e131b98be
luyi <3264812538@qq.com>
2026-03-30 09:30:37 +0800

    1

---
+++ LED.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(18,GPIO.OUT)
+
+print("LED on")
+GPIO.output(18,GPIO.HIGH)
+
+GPIO.setup(18,GPIO.OUT)
+time.sleep(1)
+
+print("LED off")
+GPIO.output(18,GPIO.LOW)
+
