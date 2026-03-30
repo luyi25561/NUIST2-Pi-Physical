512679a2b437dce44932acdc6fc4d1f98ac01e3d
luyi <3264812538@qq.com>
2026-03-30 20:11:37 +0800

    8

---
+++ Quiz2.py
@@ -0,0 +1,34 @@
+#Author:Lu Yi and Tan Zihao
+def quiz():
+    print("Welcome to the Animal Quiz!")
+    print("Answer the following questions:")
+
+    # 问题与答案
+    questions = [
+        "1. What is the largest animal on Earth?: a. Blue Whale, b. Mouse, c. Cat",
+        "2. Which bird can fly backwards?: a. Cuckoo,b.Eagle, c.Hummingbird",
+        "3. What is the only mammal capable of flight?: a. Bat, b. Squirrel, c.Dolphin "
+    ]
+    answers = [
+        "Blue whale",
+        "Hummingbird",
+        "Bat"
+    ]
+    score = 0
+
+    # 提出问题
+    for i in range(len(questions)):
+        user_answer = input(questions[i]).strip().lower()
+        if user_answer == answers[i]:
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect!")
+
+    # 输出最终得分
+    print("\nQuiz completed!")
+    print(f"You got {score}/{len(questions)} questions correct.")
+
+
+# 运行问答函数
+quiz()
